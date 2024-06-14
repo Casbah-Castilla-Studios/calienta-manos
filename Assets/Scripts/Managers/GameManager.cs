eba1bcef962d2b114ff1f248125c95a74169ce0e
alplahe <albertoplazaherraiz@gmail.com>
2024-06-14 12:48:59 +0200

    - check hand touch

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,4 +6,32 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject whiteHand;
     [SerializeField] private GameObject blackHand;
+    private Hand handAttacking;
+    private int whiteHandPoints;
+    private int blackHandPoints;
+
+    private void Awake()
+    {
+        handAttacking = Hand.Black;
+        CheckHandTouch.OnHandTouched += HandleHandTouched;
+        whiteHandPoints = 0;
+        blackHandPoints = 0;
+}
+
+    private void HandleHandTouched(Hand attackerHand, Hand defenderHand)
+    {
+        if (attackerHand != handAttacking) return;
+
+        if (attackerHand == Hand.White)
+        {
+            whiteHandPoints++;
+            Debug.Log("Punto para la mano blanca. " +  whiteHandPoints);
+        }
+
+        if (attackerHand == Hand.Black)
+        {
+            blackHandPoints++;
+            Debug.Log("Punto para la mano negra. " + blackHandPoints);
+        }
+    }
 }
